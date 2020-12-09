d4324eb269bf751d9f6b30f5c799f42092c8c265
Romano Livi <romano_livi@outlook.com>
2020-12-09 16:18:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,10 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false 
-  elsif board[index] == "X" || board[index] == "Y"
+  elsif board[index] == "X" || board[index] == "O"
     true
   end
 end
+
+
+
